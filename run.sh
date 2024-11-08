ab407418cb1ac31469703fbbed517adba9539014
Reg <69511985+RegularRabbit05@users.noreply.github.com>
2024-11-08 01:28:26 +0100

    Update run.sh

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@ PURPLE='\033[0;35m'
 RED='\033[0;31m'
 GREEN='\033[0;32m'
 NC='\033[0m'
+SP='                 '
 
 printf "\033c"
 printf "${GREEN}Starting..${NC}\n"
@@ -11,50 +12,34 @@ sleep 1
 printf "\033c"
 
 DIR=$PWD # get current dir
-# if current dir is /root print ~
-if [ "$PWD" = "/root"* ]; then
-    DIR="~${PWD#/root}"
-fi
+
+HNAME="$(hostname)"
 
 /bin/sh "/autoruns.sh"
 
-printf "\033c"
-printf "${GREEN}╭────────────────────────────────────────────────────────────────────────────────╮${NC}\n"
-printf "${GREEN}│                                                                                │${NC}\n"
-printf "${GREEN}│                             Pterodactyl VPS EGG                                │${NC}\n"
-printf "${GREEN}│                                                                                │${NC}\n"
-printf "${GREEN}│                           ${RED}© 2021 - 2024 ${PURPLE}ysdragon${GREEN}                               │${NC}\n"
-printf "${GREEN}│                                                                                │${NC}\n"
-printf "${GREEN}╰────────────────────────────────────────────────────────────────────────────────╯${NC}\n"
+printf "\033c" 
 printf "                                                                                               \n"
-printf "root@MyVPS:${DIR}#                                                                             \n"
+printf "                                                                                               \n"
+printf "${SP} ${GREEN}██╗    ██╗███████╗██╗      ██████╗ ██████╗ ███╗   ███╗███████╗${NC}\n"
+printf "${SP} ${GREEN}██║    ██║██╔════╝██║     ██╔════╝██╔═══██╗████╗ ████║██╔════╝${NC}\n"
+printf "${SP} ${GREEN}██║ █╗ ██║█████╗  ██║     ██║     ██║   ██║██╔████╔██║█████╗  ${NC}\n"
+printf "${SP} ${GREEN}██║███╗██║██╔══╝  ██║     ██║     ██║   ██║██║╚██╔╝██║██╔══╝  ${NC}\n"
+printf "${SP} ${GREEN}╚███╔███╔╝███████╗███████╗╚██████╗╚██████╔╝██║ ╚═╝ ██║███████╗${NC}\n"
+printf "${SP} ${GREEN} ╚══╝╚══╝ ╚══════╝╚══════╝ ╚═════╝ ╚═════╝ ╚═╝     ╚═╝╚══════╝${NC}\n"
+printf "                                                                                               \n"
+printf "${SP}                   ${RED}©ysdragon - ©RegDev\n"
+printf "                                                                                               \n"
+printf "                                                                                               \n"
 
 run_cmd() {
-    read -p "root@MyVPS:$DIR# " CMD
+	USNAME="$(whoami)"
+	printf "${USNAME}@${HNAME}:$DIR# \n"
+    read -p "${USNAME}@${HNAME}:$DIR# " CMD
     eval "$CMD"
-    
-    # Update DIR after executing command
     DIR=$PWD
-    if [ "$PWD" = "/root"* ]; then
-        DIR="~${PWD#/root}"
-    fi
-    
-    printf "root@MyVPS:$DIR# \n"
-    run_user_cmd
 }
 
-run_user_cmd() {
-    read -p "user@MyVPS:$DIR# " CMD2
-    eval "$CMD2"
-    
-    # Update DIR after executing command
-    DIR=$PWD
-    if [ "$PWD" = "/root"* ]; then
-        DIR="~${PWD#/root}"
-    fi
-    
-    printf "root@MyVPS:$DIR# \n"
-    run_cmd
-}
-
-run_cmd
+while true
+do
+  run_cmd
+done
